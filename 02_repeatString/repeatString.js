34742b025ca13f633d9fbb797e245f8aa2fe8435
michaelhein229 <michaelhein229@gmail.com>
2022-07-25 13:21:41 -0700

    Finished 02_repeatString, Passed 7/7 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(str, numTimes) {
+    if (numTimes < 0) {
+        return "ERROR";
+    }
+    else if (numTimes === 0) {
+        return '';
+    } else {
+        return str + repeatString(str, numTimes - 1);
+    };
+}
 
 // Do not edit below this line
 module.exports = repeatString;
