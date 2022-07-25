0acc8475dfe0ddd37b62fbedcfd416169fc09fcc
michaelhein229 <michaelhein229@gmail.com>
2022-07-25 13:34:03 -0700

    Added iterative solution to reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,18 @@
 const reverseString = function(str) {
+    /* RECURSIVE SOLUTION
     if (str === '') {
         return ''
     } else {
         return reverseString(str.substring(1)) + str.charAt(0);
+    } */
+
+    // ITERATIVE SOLUTION
+    let revStr = ''
+    while (str) {
+        revStr += str.slice(-1);
+        str = str.substr(0, str.length - 1)
     }
+    return revStr;
 };
 
 // Do not edit below this line
