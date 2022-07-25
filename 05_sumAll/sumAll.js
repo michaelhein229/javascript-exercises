2e395b29eea916d9e1cd1e77616bd185f498c089
michaelhein229 <michaelhein229@gmail.com>
2022-07-25 14:19:26 -0700

    Completed sumAll.js passed 6/6 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    if (startNum < 0 || endNum < 0) {
+        return "ERROR"
+    } else if (typeof startNum != 'number' || typeof endNum != 'number') {
+        return "ERROR"
+    } else {
+        let start = Math.min(startNum,endNum);
+        let end = Math.max(startNum, endNum);
+        sum = 0;
+        for (i = start; i <= end; i ++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
