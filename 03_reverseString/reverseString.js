1f62a9d56741e3fa9d9a29b891c724f76db7e586
michaelhein229 <michaelhein229@gmail.com>
2022-07-25 13:28:17 -0700

    Completed reverseString.js passed 4/4 tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (str === '') {
+        return ''
+    } else {
+        return reverseString(str.substring(1)) + str.charAt(0);
+    }
 };
 
 // Do not edit below this line
